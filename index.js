9d01f10b5e93d3e802518c82616dc04c0d2f0bb8
surendragt19 <surendragt2002@gmail.com>
2024-10-14 15:30:50 +0530

    origin change

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000',
+    origin:'https://chat-app-frontend-umber-eta.vercel.app',
     credentials:true
 };
 app.use(cors(corsOption)); 
